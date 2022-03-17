0cf57109ed6333471c11d8afac53750687c9e0ab
Povilas Velicka <velicka.povilas@gmail.com>
2022-03-17 20:38:22 +0200

    Update Program.cs

--- CodeAcademy/Program.cs
+++ CodeAcademy/Program.cs
@@ -1,22 +1,27 @@
-﻿{
-    class Program
+﻿using System;
+
+namespace CodeAcademy
 {
-    //Zemes rutulio plotas
-    //spindulys: 6371;
-    public static double earchArea;
-    public static double erchVolume;
-    const double PI = 3.14;
-    const double EARCH_RADIUS = 6371;
-    //Zemes turis
-
-
-    static void Main(string[] args)
+    class Program
     {
-        earchArea = PI * EARCH_RADIUS * 2;
-        erchVolume = 4 / 3 * PI * Math.Pow(EARCH_RADIUS, 2);
-        Console.WriteLine("...");
+        //Zemes rutulio plotas
+        //spindulys: 6371;
+        public static double earchArea;
+        public static double erchVolume;
+        const double PI = 3.14;
+        const double EARCH_RADIUS = 6371;
+        //Zemes turis
 
 
+        static void Main(string[] args)
+        {
+            earchArea = PI * EARCH_RADIUS * 2;
+            erchVolume = 4 / 3 * PI * Math.Pow(EARCH_RADIUS, 2);      
+
+            Console.WriteLine(
+                "Programa turi išvesti skaičiavimo rezultatą tesktiniame lange.\n" +
+                "Programos atlikimas:\n" +
+                "\n");
+        }
     }
-}
 }
