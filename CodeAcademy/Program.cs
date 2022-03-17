63e333cc78e9da744ab88ffdf1dc8d08018d7bd7
Povilas Velicka <velicka.povilas@gmail.com>
2022-03-17 20:50:30 +0200

    Update Program.cs

--- CodeAcademy/Program.cs
+++ CodeAcademy/Program.cs
@@ -30,11 +30,11 @@ namespace CodeAcademy
 
 
             Console.WriteLine("Is viso kintamuju: 3");
-            Console.WriteLine($"Kintamasis 1: {Math.Round( earchArea,2)}");
-            Console.WriteLine($"Kintamasis 2: {Math.Round(erchVolume,2)}");
-            Console.WriteLine($"Kintamasis 3: {Math.Round(izambine,2)}");
-            Console.WriteLine($"Suma: {Math.Round( earchArea + erchVolume + izambine,2)}");
-            Console.WriteLine($"Vidurkis: {Math.Round((earchArea + erchVolume + izambine)/3,2)}");
+            Console.WriteLine($"Kintamasis 1: {Math.Round( earchArea,0)}");
+            Console.WriteLine($"Kintamasis 2: {Math.Round(erchVolume,0)}");
+            Console.WriteLine($"Kintamasis 3: {Math.Round(izambine,0)}");
+            Console.WriteLine($"Suma: {Math.Round( earchArea + erchVolume + izambine,0)}");
+            Console.WriteLine($"Vidurkis: {Math.Round((earchArea + erchVolume + izambine)/3,0)}");
 
         }
 
