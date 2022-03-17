bb0467bd590099e48c65426331caa2403232db84
Povilas Velicka <velicka.povilas@gmail.com>
2022-03-17 20:34:37 +0200

    Update Program.cs

--- CodeAcademy/Program.cs
+++ CodeAcademy/Program.cs
@@ -1,24 +1,22 @@
-﻿using System;
-
-namespace CodeAcademy
-{
+﻿{
     class Program
+{
+    //Zemes rutulio plotas
+    //spindulys: 6371;
+    public static double earchArea;
+    public static double erchVolume;
+    const double PI = 3.14;
+    const double EARCH_RADIUS = 6371;
+    //Zemes turis
+
+
+    static void Main(string[] args)
     {
-        //Zemes rutulio plotas
-        //spindulys: 6371;
-        public static double earchArea;
-        const double PI = 3.14;
-        const double EARCH_RADIUS = 6371;
-        //Zemes turis
-        public static double erchVolume;
-
-        static void Main(string[] args)
-        {
-            earchArea = PI * EARCH_RADIUS * 2;
-            erchVolume = 4 / 3 * PI * Math.Pow(EARCH_RADIUS, 2);
+        earchArea = PI * EARCH_RADIUS * 2;
+        erchVolume = 4 / 3 * PI * Math.Pow(EARCH_RADIUS, 2);
+        Console.WriteLine("...");
 
 
-
-        }
     }
+}
 }
